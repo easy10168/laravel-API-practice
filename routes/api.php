b91a3b15d14ec97ef19356ad2bd4c8d47b837644
ZY Lin <easy10168@gmail.com>
2021-07-21 23:11:05 +0800

    product CRUD

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+// Route::get('/products', [ProductController::class, 'index']);
+Route::apiResource('products', ProductController::class);
+Route::get('/products/search/{name}', [ProductController::class, 'search']);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
